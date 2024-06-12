5eb0decfdee52b027be02e73bd663ce0190530c8
Jhedder <jheddersenac@gmail.com>
2024-06-12 14:17:20 -0300

    Código Python

---
+++ Aluno1.py
@@ -0,0 +1,9 @@
+class Aluno:
+    def __init__(self):
+        self.idade = 20
+
+    
+
+# Exemplo de uso da classe Aluno
+aluno1 = Aluno()
+
---
+++ Exercício Encapsulamento.py
@@ -0,0 +1,27 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def mostra_nome(self):
+        print(f"O nome desta pessoa é: {self.nome}.")
+
+    def mostra_idade(self):
+        print(f"A idade desta pessoa é: {self.idade}.")
+
+pessoa1 = Pessoa("Jhedder",20)
+pessoa1.mostra_nome()
+pessoa1.mostra_idade()
+
+pessoa2 = Pessoa("Duda",20)
+pessoa2.mostra_nome()
+pessoa2.mostra_idade()
+
+
+
+
+
+
+        
+    
+       
